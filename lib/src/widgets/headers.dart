5f5076b4c69d80a33eefd2d42a8755c6eda9f91a
paco <paco.33353@gmail.com>
2020-11-24 16:17:19 -0600

    header curvo doble

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -197,3 +197,60 @@ class _HeaderCircularPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderCircular2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderCircular2Painter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCircular2Painter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //propiedades
+
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill; // .stroke  .fill
+    paint.strokeWidth = 2;
+
+    final path = new Path();
+
+    // dibujar con el path y el lapiz
+    //path.moveTo(0, size.height * 0.35);
+    path.lineTo(0, size.height * 0.4);
+    
+    path.quadraticBezierTo(
+        //punto a atravezar
+        size.width * 0.25,
+        size.height * 0.5,
+        //punto final
+        size.width * 0.5,
+        size.height * 0.4);
+
+    path.quadraticBezierTo(
+        //punto a atravezar
+        size.width * 0.75,
+        size.height * 0.3,
+        //punto final
+        size.width,
+        size.height * 0.4);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}

0cec97ca837d09a469956747046cce587aaaf6d1
paco <paco.33353@gmail.com>
2020-11-24 16:09:12 -0600

    Header curvo

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -153,3 +153,47 @@ class _HeaderPicoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderCircular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderCircularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCircularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //propiedades
+
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill; // .stroke  .fill
+    paint.strokeWidth = 10;
+
+    final path = new Path();
+
+    // dibujar con el path y el lapiz
+    //path.moveTo(0, size.height * 0.35);
+    path.lineTo(0, size.height * 0.3);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.5, size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    // path.lineTo(size.width, size.height * 0.3);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}

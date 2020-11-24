c8ff6b964dc7440b5a720eb5897c8d064dddc02b
paco <paco.33353@gmail.com>
2020-11-24 15:14:12 -0600

    header triangular

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -62,7 +62,49 @@ class _HeaderDiagonalPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class HeaderTriangular extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //propiedades
+
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill; // .stroke  .fill
+    paint.strokeWidth = 2;
+
+    final path = new Path();
+
+    // dibujar con el path y el lapiz
+    //path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    //path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
 }
